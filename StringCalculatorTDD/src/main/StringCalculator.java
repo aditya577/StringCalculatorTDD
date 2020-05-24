3822afff4f20092ee0d9b818ee9da494800f6991
aditya <aditya@Warlord-pc>
2020-05-25 00:18:16 +0530

    made some more changes but still not paased

--- StringCalculatorTDD/src/main/StringCalculator.java
+++ StringCalculatorTDD/src/main/StringCalculator.java
@@ -3,11 +3,25 @@ package main;
 public class StringCalculator {
 
 	public int add(String str) {
-		int sum =0;
+		int sum;
 		
 		if(str.equals(""))
 			sum = 0;
+		
+		else {
 			
+			String[] numArray = str.split(",");
+			
+			int temp =0;
+			for(String num : numArray)
+			{
+				temp += Integer.parseInt(num);
+			}
+			sum = temp;
+			
+		}
+		
+		System.out.println("sum:"+sum);
 		return sum;
 	}
 
